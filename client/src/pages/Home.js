8aa2ccda2040578dba40fccc5e31195d3574f92f
keneversley <krazorfbx@gmail.com>
2022-06-07 08:21:40 -0700

    thank

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,28 +1,7 @@
-import React from 'react';
-import { useQuery } from '@apollo/client';
-import { QUERY_THOUGHTS } from '../utils/queries';
-import ThoughtList from '../components/ThoughtList';
-
-const Home = () => {
-
-  const { loading, data } = useQuery(QUERY_THOUGHTS);
-  const thoughts = data?.thoughts || [];
-console.log(thoughts);
-
-
-  return (
-    <main>
-  <div className="flex-row justify-space-between">
-    <div className="col-12 mb-3">
-      {loading ? (
-        <div>Loading...</div>
-      ) : (
-        <ThoughtList thoughts={thoughts} title="Some Feed for Thought(s)..." />
-      )}
-    </div>
-  </div>
-</main>
-  );
-};
-
-export default Home;
+import React from 'react'; import ThoughtList from '../components/ThoughtList'; import FriendList from '../components/FriendList'; import Auth from '../utils/auth'; import { useQuery } from '@apollo/client'; import { QUERY_THOUGHTS, QUERY_ME_BASIC } from '../utils/queries'; const Home = () => { // use useQuery hook to make query request const { loading, data } = useQuery(QUERY_THOUGHTS); // use object destructuring to extract `data` from the `useQuery` Hook's response and rename it `userData` to be more descriptive const { data: userData } = useQuery(QUERY_ME_BASIC); const thoughts = data?.thoughts || []; console.log(thoughts); const loggedIn = Auth.loggedIn(); return (
+  {loading ? (
+  Loading...
+  ) : ( )}
+  {loggedIn && userData ? (
+  ) : null}
+  ); }; export default Home;
